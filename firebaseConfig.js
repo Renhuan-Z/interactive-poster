5a2f38ec35d6cb9feebabb6d48658ce2917628a1
Renhuan-Z <128282180+Renhuan-Z@users.noreply.github.com>
2024-05-27 17:50:28 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,19 @@
-// firebaseConfig.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCC4xyp4HniwI46YFJoWf-BgjyhtDia18o",
-    authDomain: "interactive-poster-dad2f.firebaseapp.com",
-    projectId: "interactive-poster-dad2f",
-    storageBucket: "interactive-poster-dad2f.appspot.com",
-    messagingSenderId: "759472690939",
-    appId: "1:759472690939:web:ad38fb62a9f1908a08025e"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
-  
+  apiKey: "AIzaSyCC4xyp4HniwI46YFJoWf-BgjyhtDia18o",
+  authDomain: "interactive-poster-dad2f.firebaseapp.com",
+  projectId: "interactive-poster-dad2f",
+  storageBucket: "interactive-poster-dad2f.appspot.com",
+  messagingSenderId: "759472690939",
+  appId: "1:759472690939:web:ad38fb62a9f1908a08025e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
