00dea7e71d2df8afff0d5e8b70114fc3a2fe6cd9
Renhuan-Z <128282180+Renhuan-Z@users.noreply.github.com>
2024-05-30 00:29:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-
-// Firebase 配置信息
-const firebaseConfig = {
-  apiKey: "AIzaSyCC4xyp4HniwI46YFJoWf-BgjyhtDia18o",
-  authDomain: "interactive-poster-dad2f.firebaseapp.com",
-  projectId: "interactive-poster-dad2f",
-  storageBucket: "interactive-poster-dad2f.appspot.com",
-  messagingSenderId: "759472690939",
-  appId: "1:759472690939:web:c837fba2dbad00a208025e"
-};
-
-// 初始化 Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
 const canvas = document.getElementById('drawing-canvas');
 const context = canvas.getContext('2d');
 let drawing = false;
@@ -240,3 +225,4 @@ async function loadDrawings() {
 }
 
 window.onload = loadDrawings;
+
