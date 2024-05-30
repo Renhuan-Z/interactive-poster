e7f431a1f6791bbc398e8ccfe6be96d7bdca613b
Renhuan-Z <128282180+Renhuan-Z@users.noreply.github.com>
2024-05-30 05:28:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCC4xyp4HniwI46YFJoWf-BgjyhtDia18o",
+    authDomain: "interactive-poster-dad2f.firebaseapp.com",
+    projectId: "interactive-poster-dad2f",
+    storageBucket: "interactive-poster-dad2f.appspot.com",
+    messagingSenderId: "759472690939",
+    appId: "1:759472690939:web:c837fba2dbad00a208025e"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
 const canvas = document.getElementById('drawing-canvas');
 const context = canvas.getContext('2d');
 let drawing = false;
