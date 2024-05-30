7907f55e60ed6c7a98585f3d25effb4d1276274e
Renhuan-Z <128282180+Renhuan-Z@users.noreply.github.com>
2024-05-30 05:04:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCC4xyp4HniwI46YFJoWf-BgjyhtDia18o",
-  authDomain: "interactive-poster-dad2f.firebaseapp.com",
-  projectId: "interactive-poster-dad2f",
-  storageBucket: "interactive-poster-dad2f.appspot.com",
-  messagingSenderId: "759472690939",
-  appId: "1:759472690939:web:c837fba2dbad00a208025e"
-};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
