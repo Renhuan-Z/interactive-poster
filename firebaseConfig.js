89a619b8e29e4b559d77ac43946079dce0f6b399
Renhuan-Z <128282180+Renhuan-Z@users.noreply.github.com>
2024-05-30 04:01:40 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
+// Firebase 配置
 const firebaseConfig = {
-apiKey: "AIzaSyCC4xyp4HniwI46YFJoWf-BgjyhtDia18o",
-  authDomain: "interactive-poster-dad2f.firebaseapp.com",
-  projectId: "interactive-poster-dad2f",
-  storageBucket: "interactive-poster-dad2f.appspot.com",
-  messagingSenderId: "759472690939",
-  appId: "1:759472690939:web:c837fba2dbad00a208025e"
+    apiKey: "AIzaSyCC4xyp4HniwI46YFJoWf-BgjyhtDia18o",
+    authDomain: "interactive-poster-dad2f.firebaseapp.com",
+    projectId: "interactive-poster-dad2f",
+    storageBucket: "interactive-poster-dad2f.appspot.com",
+    messagingSenderId: "759472690939",
+    appId: "1:759472690939:web:c837fba2dbad00a208025e"
 };
 
 // 初始化 Firebase
